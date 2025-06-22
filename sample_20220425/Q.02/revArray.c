ae1e7c3f4780e0e243aa0638e4e01f2097565f53
ekusy758 <mano7584@yahoo.co.jp>
2025-06-22 17:32:21 +0900

    2025.06.22 [Q2] revArray init

---
+++ sample_20220425/Q.02/revArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int array[5] = {1, 2, 3, 4, 5};
+  int right, left;
+  int tmp;
+  size_t length = sizeof(array) / sizeof(array[0]);
+
+  for (left = 0; left < length / 2; left++) {
+    right = length - left - 1;
+    tmp = array[right];
+    array[right] = array[left];
+    array[left] = tmp;
+  }
+
+  printf("Reversed array: ");
+  for (int i = 0; i < length; i++) {
+    printf("%d ", array[i]);
+  }
+
+  return 0;
+}

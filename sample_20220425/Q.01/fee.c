76a6f86c16dd4d4997508b81a3f387c70841a474
ekusy758 <mano7584@yahoo.co.jp>
2025-06-22 17:11:20 +0900

    2025.06.22 問題に合わせて戻り値を変数retに修正

--- sample_20220425/Q.01/fee.c
+++ sample_20220425/Q.01/fee.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 int fee(int age) {
-    if (age < 0) return -1;
-
-    if (age <= 3) {
-        return 100;
-    } else if (age <= 9) {
-        return 300;
-    } else {
-        return 500;
-    }
+  int ret = 0;
+  if (age <= 3) {
+    ret = 100;
+  } else if (age <= 9) {
+    ret = 300;
+  } else {
+    ret = 500;
+  }
+  return ret;
 }
 
 int main(int argc, char **argv) {
-    if (argc == 2) {
-        printf("age %s: %d yen.\n", argv[1], fee(atoi(argv[1])));
-    }
-    return 0;
+  if (argc == 2) {
+    printf("age %s: %d yen.\n", argv[1], fee(atoi(argv[1])));
+  }
+  return 0;
 }

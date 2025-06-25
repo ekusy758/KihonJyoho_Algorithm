b931ebc6b6cca0919f8ee7b0d8e78c18d27e7028
ekusy758 <mano7584@yahoo.co.jp>
2025-06-26 00:15:44 +0900

    2025.06.26 Q2 微調整＆実行イメージ追加

--- sample_20220425/Q.02/program.c
+++ sample_20220425/Q.02/program.c
@@ -6,17 +6,19 @@ int main(int argc, char **argv) {
   int tmp;
   size_t length = sizeof(array) / sizeof(array[0]);
 
-  for (left = 0; left < length / 2; left++) {
+  for (left = 0; left < (length / 2); left++) {
     right = length - left - 1;
     tmp = array[right];
     array[right] = array[left];
     array[left] = tmp;
   }
 
-  printf("Reversed array: ");
+  printf("Reversed array: {");
   for (int i = 0; i < length; i++) {
-    printf("%d ", array[i]);
+    printf("%d", array[i]);
+    if (i < length - 1) printf(", ");
   }
+  printf("}\n");
 
   return 0;
 }

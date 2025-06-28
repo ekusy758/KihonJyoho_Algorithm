0607cfd13622ab0ed68fb5d2e49ba3c867c8c4b5
ekusy758 <mano7584@yahoo.co.jp>
2025-06-29 00:37:33 +0900

    2025.06.29 Q2 プログラムフロー図

---
+++ sample_20220425/Q.02/プログラムフロー図/gen_graph_Q2.py
@@ -0,0 +1,23 @@
+from graphviz import Digraph
+
+dot = Digraph(format='png')
+dot.attr(rankdir='TB')
+dot.attr('node', shape='box', style='filled', fontname='MS Gothic', fillcolor='lightyellow')
+
+dot.node('start', '開始')
+dot.node('init', '配列を初期化 {1,2,3,4,5}')
+dot.node('cond', '左端の位置を0とする;\n要素数÷2の商より小さい?', shape='diamond', fillcolor='lightblue')
+dot.node('calc', '右端の位置を取得(要素数-左端の位置)')
+dot.node('swap', '配列の右端と左端の要素を交換')
+dot.node('inc', '左端の位置を1増やす')
+dot.node('end', '終了')
+
+dot.edge('start', 'init')
+dot.edge('init', 'cond')
+dot.edge('cond', 'calc', label='Yes')
+dot.edge('calc', 'swap')
+dot.edge('swap', 'inc')
+dot.edge('inc', 'cond') 
+dot.edge('cond', 'end', label='No')
+
+dot.render('graph_Q2', cleanup=True)

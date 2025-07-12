eabce37f1a4d5c334c9e616beb7964ba492bc34b
ekusy758 <mano7584@yahoo.co.jp>
2025-07-12 22:01:49 +0900

    2025.07.12 Q15 by Node.js Vite + React

---
+++ sample_20221226/Q.15/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { calculateWinner, removeUncheckedClass } from './utils.js';
+
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onClick }) {
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'O' : 'X');
+  }
+
+  return (
+    <div className="square-box hidden">
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={onClick(0)} />
+        <Square value={squares[1]} onSquareClick={onClick(1)} />
+        <Square value={squares[2]} onSquareClick={onClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={onClick(3)} />
+        <Square value={squares[4]} onSquareClick={onClick(4)} />
+        <Square value={squares[5]} onSquareClick={onClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={onClick(6)} />
+        <Square value={squares[7]} onSquareClick={onClick(7)} />
+        <Square value={squares[8]} onSquareClick={onClick(8)} />
+      </div>
+    </div>
+  );
+}
+
+export default function Game() {
+  const [squares, setSquares] = useState(['X', 'O', 'X', 'X', 'O', 'O', null, null, null]);
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'O' : 'X';
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+
+    removeUncheckedClass(nextSquares);
+  }
+
+  return (
+    <>
+      <div className='center-board root'>
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+      </div>
+      <hr/>
+      <div className='center-board Unchecked'>
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+      </div>
+      <hr/>
+      <div className='center-board Unchecked'>
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+      </div>
+      <hr/>
+      <div className='center-board Unchecked'>
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+        <Board xIsNext={xIsNext} squares={squares} onClick={(i) => () => handleClick(i)} />
+      </div>
+    </>
+  )
+}
---
+++ sample_20221226/Q.15/src/utils.js
@@ -0,0 +1,71 @@
+let direction = null;
+
+export function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export function removeUncheckedClass(nextSquares) {
+  const r = ['X', 'O', 'X', 'X', 'O', 'O', 'O', null, null];
+  const c = ['X', 'O', 'X', 'X', 'O', 'O', null, 'O', null]; // Winner 'O'
+  const l = ['X', 'O', 'X', 'X', 'O', 'O', null, null, 'O'];
+  const rr = [
+    ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', null],
+    ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'O'],
+  ];
+  const rl = [
+    ['X', 'O', 'X', 'X', 'O', 'O', 'O', null, 'X'],
+    ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'O', 'X'], // Winner 'O'
+  ];
+  const lr = [
+    ['X', 'O', 'X', 'X', 'O', 'O', 'X', null, 'O'], // Winner 'X'
+  ];
+  const ll = [
+    ['X', 'O', 'X', 'X', 'O', 'O', null, 'X', 'O'],
+    ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'O'],
+  ];
+  const uncheckedBoard = document.querySelector('.Unchecked');
+  uncheckedBoard.classList.remove('Unchecked');
+  const childBoard = uncheckedBoard.children;
+  if (arraysEqual(nextSquares, r)) {
+    childBoard[0].classList.remove('hidden');
+    direction = 'right';
+  } else if (arraysEqual(nextSquares, c)) {
+    childBoard[1].classList.remove('hidden');
+  } else if (arraysEqual(nextSquares, l)) { 
+    childBoard[2].classList.remove('hidden');
+    direction = 'left';
+  } else if (direction === 'right' && rr.some(pattern => arraysEqual(nextSquares, pattern))) {
+    childBoard[0].classList.remove('hidden');
+  } else if (rl.some(pattern => arraysEqual(nextSquares, pattern))) {
+    childBoard[1].classList.remove('hidden');
+  } else if (lr.some(pattern => arraysEqual(nextSquares, pattern))) {
+    childBoard[2].classList.remove('hidden');
+  } else if (direction === 'left' && ll.some(pattern => arraysEqual(nextSquares, pattern))) {
+    childBoard[3].classList.remove('hidden');
+  }
+}
+
+function arraysEqual(a, b) {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, i) => val === b[i])
+  );
+}

d87f00a8e82222dfe1f4be383ac9b9f317d5c120
ekusy758 <mano7584@yahoo.co.jp>
2025-07-12 23:19:54 +0900

    デプロイ用にコンフィグ修正

--- sample_20221226/Q.15/vite.config.js
+++ sample_20221226/Q.15/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/KihonJyoho_Algorithm/sample_20221226/Q.15/', // GitHub Pagesでの相対パスを追加
+  base: '/KihonJyoho_Algorithm/', // リポジトリ名と同じにする
   plugins: [react()]
 });
